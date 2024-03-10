8b1eb8fc99871a1edf17663fefd14633dfd8f726
Flesh <olivia.a.gowan@gmail.com>
2024-03-10 14:24:50 -0400

    finding where I messed up the code

--- controllers/thoughtController.js
+++ controllers/thoughtController.js
@@ -23,23 +23,30 @@ module.exports = {
       return res.status(400).json(err);
     }
   },
+
   // create a new thought
   async createThought(req, res) {
     try {
-      // create the thought
-      const thought = await Thought.create(req.body);
-      // update the user's thoughts array
-      const user = await User.findOne({ _id: req.body.userId });
-      if (thought) {
-        console.log(thought);
-        user.thoughts.push(thought._id);
-        await user.save();
-      }
-      return res.json(thought);
-    } catch (err) {
-      return res.status(400).json(err);
+    // create the thought
+    const thought = await Thought.create(req.body);
+    const reactions = req.body.reactions;
+    if (!reactions) {
+      return reactions = [];
     }
+    // update the user's thoughts array
+    const user = await User.findOne({ _id: req.body.userId });
+    if (thought) {
+      console.log(thought);
+      user.thoughts.push(thought._id);
+      await user.save();
+    }
+    return res.json(thought);
+  } catch (err) {
+    return res.status(400).json(err);
+  }
   },
+
+     
   // update thought by id
   async updateThought(req, res) {
     try {
@@ -84,34 +91,38 @@ module.exports = {
       return res.status(400).json(err);
     }
   },
+
   // add reaction to thought
-  async addReaction(req, res) {
+  async addReaction({ params, body}, res) {
     try {
-      const thought = await Thought.findOneAndUpdate(
-        { _id: req.params.thoughtId },
-        { $addToSet: { reactions: req.body } },
-        { new: true, runValidators: true }
-      );
-      res.json(thought);
-    } catch (err) {
-      res.status(400).json(err);
-    }
-  },
-  // remove reaction from thought
-  async removeReaction(req, res) {
-    try {
-      const thought = await Thought.findOneAndUpdate(
-        { _id: req.params.thoughtId },
-        { $pull: { reactions: req.params.reactionId } },
-        { new: true }
-      );
+      const thought = await Thought.findByIdAndUpdate( params.thoughtId,
+        {$push: { reactions: body }},
+        { new: true, runValidators: true});
 
       if (!thought) {
-        res.status(404).json({ message: 'no thoughts here' });
+        return res.status(404).json({ message: 'no thoughts here' });
       }
-      res.json(thought);
+      return res.json(thought);
     } catch (err) {
-      res.status(400).json(err);
+      return res.status(400).json(err);
+    }
+  },
+
+  // remove reaction from thought
+  async removeReaction({ params }, res) {
+    try {
+      const thought = await Thought.findByIdAndUpdate(
+          params.thoughtId,
+          { $pull: { reactions: { reactionId: params.reactionId } } },
+          { new: true }
+        )
+  
+      if (!thought) {
+        return res.status(404).json({ message: 'No thoughts here' });
+      }
+      return res.json(thought);
+    } catch (err) {
+      return res.status(400).json(err);
     }
   }
-};
+}; 
--- models/Thought.js
+++ models/Thought.js
@@ -1,6 +1,28 @@
 const { Schema, model } = require('mongoose');
-const reactionSchema = require('./Reaction');
 const moment = require('moment');
+
+
+const reactionSchema = new Schema(
+  {
+    reactionId: {
+      type: Schema.Types.ObjectId,
+      default: () => new Types.ObjectId()
+    },
+    reactionBody: {
+      type: String,
+      min: 1,
+      max: 230
+    },
+    userId: {
+      type: Schema.Types.ObjectId,
+      ref: 'User',
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+    }
+  }
+);
 
 const thoughtSchema = new Schema(
   {
@@ -23,6 +45,7 @@ const thoughtSchema = new Schema(
   {
     toJSON: {
       virtuals: true,
+      getters: true,
     },
     id: false,
   }
@@ -31,6 +54,11 @@ const thoughtSchema = new Schema(
 // format data
 thoughtSchema.virtual('Date').get(function() {
   return moment(thoughtSchema.createdAt).format('MMMM, DD, YYYY');
+});
+
+// get reaction count 
+thoughtSchema.virtual('reactionCount').get(function() {
+  return this.reactions.length;
 });
 
 const Thought = model('Thought', thoughtSchema);
